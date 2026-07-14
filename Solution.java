bc03112e13d7fdad9b6ba0e3c3df84e3ffb027db
stlawton <lawtons347@gmail.com>
2026-07-14 21:27:10 +0000

    Created isPalindrome method to test is substring is a Palindrome

--- Solution.java
+++ Solution.java
@@ -1,5 +1,22 @@
 class Solution {
     public String longestPalindrome(String s) {
-        
+        for(int i = 0; i < s.length(); i++){
+            
+        }
+    }
+
+    public Boolean isPalindrome(String s){
+        int front = 0;
+        int end = (s.length() - 1);
+
+        while (front < end){
+            if (s.charAt(front) != s.charAt(end)){
+                return false;
+            }
+
+            front += 1;
+            end -= 1;
+        }
+        return true;
     }
 }
