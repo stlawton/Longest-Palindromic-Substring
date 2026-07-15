6c9ea0feca06644f8aa04ad001984e43df2a5476
stlawton <lawtons347@gmail.com>
2026-07-15 15:08:45 +0000

    Implement longestPalindrome method to find the longest palindromic substring

--- Solution.java
+++ Solution.java
@@ -1,8 +1,19 @@
 class Solution {
     public String longestPalindrome(String s) {
+        String sub = "";
+        String subMax = "";
+        int max = 0;
         for(int i = 0; i < s.length(); i++){
-            
+            for(int j = i; j < s.length(); j++){
+                sub += s.charAt(j);
+                if (isPalindrome(sub) && sub.length() > max){
+                    max = sub.length();
+                    subMax = sub;
+                }
+            }
         }
+
+        return subMax;
     }
 
     public Boolean isPalindrome(String s){
