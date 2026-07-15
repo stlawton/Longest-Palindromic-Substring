5046cda9b25de3b83178d78f5139708f51592f6d
stlawton <lawtons347@gmail.com>
2026-07-15 15:31:41 +0000

    Fix substring reset in longestPalindrome method and add Test class for example cases

--- Solution.java
+++ Solution.java
@@ -11,6 +11,7 @@ class Solution {
                     subMax = sub;
                 }
             }
+            sub = "";
         }
 
         return subMax;
---
+++ Test.java
@@ -0,0 +1,7 @@
+class Test {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.longestPalindrome("babad")); // Output: "bab" or "aba"
+        System.out.println(solution.longestPalindrome("cbbd"));  // Output: "bb"
+    }
+}
